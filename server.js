20ed4aa8e219906eec84fe820d1d3747f86d10fa
System <none>
2017-06-30 22:56:41 +0000

    🐉💋 Updated with Glitch

--- server.js
+++ server.js
@@ -22,14 +22,46 @@ app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
 
+// the webcam info
+//   url: base url to the image, ignoring the timestamp (seconds is problematic)
+//   coords: rough guess as to the location of the webcam
+//   crop: (min-x, min-y, max-x, max-y) to serve as a very basic map 
+//         to determine sky color
+// image dims: 1024 × 768
+// also try to clip out the camera header/watermark (it is blue
+//   maybe affects the palette at least)
+const webcams = {
+  "alpine": {
+    url: "https://www.nps.gov/webcams-romo/alpine_visitor_center.jpg",
+    coords: [40.441070, -105.753852],
+    crop: [0, 12, 1024, 180]
+  },
+  "longspeak": {
+    url: "https://www.nps.gov/webcams-romo/longs_peak.jpg",
+    coords: [40.282477, -105.542896], // a guess based on the desc.
+    crop: [0, 12, 1024, 190]
+  },
+  "divide": {
+    url: "https://www.nps.gov/webcams-romo/glacier_basin.jpg",
+    coords: [40.328202, -105.595765],
+    crop: [0, 12, 830, 290]
+  }
+}
+
 app.get("/weather", function (request, response) {
+  // query params:
+  //  webcam: longspeak || alpine || divide
+  //  url: a url to override the live webcam (for testing)
+  //
+  // the webcams update every ten minutes, at 1 past(?)
+  //
+  // return the dominant color, color palette, and 
+  //   whether the dominant color is likely to be weather-related
+  //   ie, not blue skies
+  
   var webcam = request.query.webcam || "divide";
-  
-  // at some point, want to timebox the request
-  // console.log(_goldenhour(webcam));
-  
-  // a test imge
-  var url = 'https://cdn.glitch.com/a2065909-acd9-44c7-8be4-b37d17dee6ef%2Fglacier_basin_overcast.jpg?1498765829796';
+  var url = request.query.url || webcams[webcam].url;
+ 
   var Client = new client();
   var req = Client.get(url,
     undefined,
@@ -57,9 +89,14 @@ app.get("/weather", function (request, response) {
         console.log(chroma(dom).hex());
         console.log(palette);
         
-        _close_to_blue(dom);
+        var am_i_weather = _close_to_blue(dom);
+        console.log('am i weather? ', am_i_weather);
         
-        response.json({"dominant": chroma(dom).hex(), "palette": palette.map(p => chroma(p).hex())});
+        response.json({
+          "dominant": chroma(dom).hex(), 
+          "palette": palette.map(p => chroma(p).hex()),
+          "weather": am_i_weather
+        });
         
       }).catch(function(err) {
         console.log('jimp error: ', err);
@@ -74,39 +111,50 @@ function _close_to_blue(color) {
   // something to test with since it is cloudy
   // #7CB3EC lighter rgb(124, 179, 236)
   // #567DC2 darker rgb(86, 125, 194)
+  // co valley: #466FAF, #3E6699, #3F6DAB, #5F87C4
   
+  // for testing the diff calcs
   // red rgb(230, 24, 24)
-  // yellow rgb(230, 186, 24)
+  // yellow rgb(230, 186,n 24)
   // a green rgb(24, 230, 141)
-  // mmore turquoise rgb(8, 246, 226)
+  // more turquoise rgb(8, 246, 226)
   
   // everything to lab colors
   // as {L: , A: , B: }
   var src_lab = chroma(color).lab();
   var src = {L: src_lab[0], A: src_lab[1], B: src_lab[2]};
   
-  // tmp sorrows
-  var blues = [[124, 179, 236], [86, 125, 194], [230, 24, 24], [230, 186, 24], [24, 230, 141], [8, 246, 226]].map(c => { var lab = chroma(c).lab(); return {L: lab[0], A: lab[1], B: lab[2]}});
+  // tmp sorrows (this includes a red, yellow, and greens)
+  // var blues = [[124, 179, 236], [86, 125, 194], [230, 24, 24], [230, 186, 24], [24, 230, 141], [8, 246, 226]].map(c => { var lab = chroma(c).lab(); return {L: lab[0], A: lab[1], B: lab[2]}});
   
-  for (var b in blues) {
-    console.log(b, delta.getDeltaE00(src, blues[b]));
-  }
+  var blues = [
+    [124, 179, 236], 
+    [86, 125, 194],
+    [70, 111, 175],
+    [62, 102, 153],
+    [95, 135, 196],
+    [87, 93, 126]
+  ].map(
+    c => { var lab = chroma(c).lab(); return {L: lab[0], A: lab[1], B: lab[2]}}
+  );
   
-  return false;
+  // if any is similar to the dominant color,
+  // it's not weather
+  // where similar = delta-e < 5?
+  // (5 is not high enough)
+  return blues.some(b => {
+    return delta.getDeltaE00(src, b) > 10.;
+  });
 }
 
-const webcam_coords = {
-  "alpine": [40.441070, -105.753852],
-  "longspeak": [40.282477, -105.542896], // a guess based on the desc.
-  "divide": [40.328202, -105.595765]
-};
+
 
 function _goldenhour(webcam) {
   // so check the webcam from the start of golden hour
   // to dusk for mountain time
   var d = Date.parse(new Date().toLocaleString("en-US", {timeZone: "America/Denver"}));
   
-  var calc = suncalc.getTimes(d, webcam_coords[webcam][0], webcam_coords[webcam][1]);
+  var calc = suncalc.getTimes(d, webcams[webcam].coords[0], webcams[webcam].coords[1]);
   
   console.log(calc);
   
